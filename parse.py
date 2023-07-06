177e7ab4a8683205320f25eb5090fd501a3a7d7a
Jaromir Blutaumüller <jaromirblu@gmail.com>
2023-07-06 14:28:23 +0200

    basic installation using pacman

--- parse.py
+++ parse.py
@@ -1,5 +1,11 @@
 import tomllib
+import subprocess
 from typing import Any
+
+
+# supported managers
+MAN_PACMAN: str = "pacman"
+MAN_APT: str = "apt"
 
 
 class Wharf():
@@ -9,6 +15,33 @@ class Wharf():
 
     def __str__(self) -> str:
         return f"Wharf[manager=\"{self.manager}\", names={self.names}]"
+    
+    def install(self) -> bool:
+        """ install packages using manager """
+
+        command: list[str] = []
+        if self.manager == MAN_PACMAN:
+            command = ["pacman", "-Sy", "--noconfirm"]
+            command.extend(self.names)
+
+        elif self.manager == MAN_APT: # TODO testing
+            command = ["apt", "install", "-y"]
+            command.extend(self.names)
+
+        else:
+            print(f"package manager '{self.manager}' not supported")
+            return False
+
+        try:
+            res = subprocess.run(command)
+            if res.returncode == 0:
+                return False
+        except Exception:
+            print(f"Make sure {self.manager} is installed and you are running Wharf as root.")
+            return False
+            
+
+        return True
 
 
 def parse_toml(path: str) -> dict[str, Any]:
--- wharf.py
+++ wharf.py
@@ -2,14 +2,20 @@ from typing import Any
 import parse
 
 def main():
-    print("wharf")
+    # parse file and build wharf object
+    parsed: dict[str, Any] = parse.parse_toml("wharf.toml")
+    if parsed == None:
+        exit(1)
+
+    wharf: parse.Wharf = parse.build_wharf(parsed)
+
+    # install packages
+    success: bool = wharf.install()
+    if success:
+        print("installation successful.")
+    else:
+        exit(1)
 
 
 if __name__ == "__main__":
     main()
-
-    # parse file and build wharf object
-    parsed: dict[str, Any] = parse.parse_toml("wharf.toml")
-    wharf: parse.Wharf = parse.build_wharf(parsed)
-
-    print(wharf)
