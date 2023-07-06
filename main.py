6dd3f0623ded30207ee6d6a295c2eddd1e666d79
Jaromir Blutaumüller <jaromirblu@gmail.com>
2023-07-06 15:05:28 +0200

    print usage

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 from typing import Any
+import sys
 import parse
 import wharf
+import usage
 
 
 def main():
+    if len(sys.argv) == 2:
+        operation: str = sys.argv[1]
+
+        if operation == "-h" or operation == "help":
+            usage.print_usage()
+            exit(0)
+        elif operation == "-n" or operation == "new":
+            # TODO generate new "wharf.toml"
+            print("generating new wharf.toml file")
+            exit(0)
+        else:
+            print(f"invalid argument '{operation}'")
+            exit(1)
+
+
     # parse file and build wharf object
     WHARF_PATH: str = "wharf.toml"
     parsed: dict[str, Any] = parse.parse_toml(WHARF_PATH)
---
+++ usage.py
@@ -0,0 +1,12 @@
+
+
+def print_usage():
+    """ print help and usage """
+
+    print("USAGE:")
+    print("Install packages:")
+    print("1. move to dir containing 'wharf.toml'")
+    print("2. run 'sudo ./main.py' to install\n")
+
+    print("Create new 'wharf.toml':")
+    print("1. run 'sudo main.py new' or 'sudo main.py -n'")
